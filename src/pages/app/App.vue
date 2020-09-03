64c535c02fa56938165c1214306a81ad17ba3b81
Yuehao Wang <352209095@qq.com>
2020-09-03 17:31:16 +0800

    添加云顶之弈小工具

--- src/pages/app/App.vue
+++ src/pages/app/App.vue
@@ -1,29 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <router-view></router-view>
+    <p>需要<input type="num" v-model="groupNum">组数据</p>
+    <p>每组数据<input type="num" v-model="gronpLength">条</p>
+    <p>完整数据区间<input type="num" v-model="fullMin">~<input type="num" v-model="fullMax"></p>
+    <p>合格数据区间<input type="num" v-model="qualifiedMin">~<input type="num" v-model="qualifiedMax"></p>
+    <p>合格率范围<input type="num" v-model="passRateMin">~<input type="text" v-model="passRateMax"></p>
+    <p><input type="button" value="生成数据" @click="getNumList"></p>
+    <table id="sjs-table" border="1" cellspacing="0" cellpadding="0">
+      <thead>
+      <tr>
+        <td :colspan="gronpLength">数据</td>
+        <td>合格率</td>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item,index) in groupNumList" :key="index">
+        <td v-for="(_item,_index) in item.list" :key="_index">{{ _item }}</td>
+        <td>{{ item.rate }}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import XLSX from 'xlsx'
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      groupNum: 100,
+      gronpLength: 10,
+      fullMin: -10,
+      fullMax: 20,
+      qualifiedMin: 0,
+      qualifiedMax: 10,
+      passRateMin: 0.8,
+      passRateMax: 1,
+      groupNumList: [],
+    }
+  },
+  methods: {
+    getNumList() {
+      // 确定合格率
+      this.groupNumList = []
+      const tempRate = (Number(this.passRateMax) - Number(this.passRateMin)) * 10 + 1
+      for (let i = 0; i < Number(this.groupNum); i++) {
+        const passRate = parseInt(Math.random() * tempRate + Number(this.passRateMin) * 10) / 10
+        console.log("合格率" + passRate)
+        this.groupNumList.push({
+          list: this.getGroupNum(passRate),
+          rate: passRate
+        })
+      }
+      setTimeout(()=>{
+        var wb = XLSX.utils.table_to_book(document.getElementById('sjs-table'));
+        XLSX.writeFile(wb, "export.xlsx");
+      },2000)
+      // console.log(this.groupNumList)
+    },
+    // 获取一组数据
+    getGroupNum(passRate) {
+      const arr = []
+      for (let i = 0; i < passRate * 10; i++) {
+        arr.push((Math.random() * (Number(this.qualifiedMax) - Number(this.qualifiedMin)) + Number(this.qualifiedMin)).toFixed(2))
+      }
+      while (arr.length < this.gronpLength) {
+        const num = parseFloat(Math.random() * (Number(this.fullMax) - Number(this.fullMin)) + Number(this.fullMin)).toFixed(2)
+        if (!(num >= Number(this.qualifiedMin) && num <= Number(this.qualifiedMax))) {
+          arr.push(num)
+        }
+      }
+      console.log(arr)
+      // 进行乱序
+      return arr.sort((a, b) => .5 - Math.random())
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
